066a6dea9895d8240f7e0f7520154401c4abd207
Lucas Rosa <lucas.rosa@tradesis.com.br>
2023-06-26 08:40:11 -0300

    Update cardImg3col-form.php

--- secoes/card/cardImg3col-form.php
+++ secoes/card/cardImg3col-form.php
@@ -26,13 +26,7 @@ $parametros = json_decode($secoesPagina['parametros'], true);
             <input type="text" name="subTitulo" class="form-control" value="<?php echo $parametros["subTitulo"] ?>">
         </div>
     </div>
-<!--     <div class="row" style="margin-top: 50px;">
-        <div class="col-sm-6">
-            <label class='control-label' for='inputNormal' style="margin-top: -20px;">imagens</label>
-            <textarea name="listas" id="" cols="130" rows="7"><?php echo $secoesPagina['listas'] ?></textarea>
-        </div>
-    </div> -->
-
+    
     <div class="row form-group">
         <div class="col-sm-4" style="margin-top: 40px">
             <label class='control-label' for='inputNormal' style="margin-top: -20px;">Imagem1</label>
